06d9c680237a6916b6b3d49016457d5741d99762
Le Quan <quanletrong@admicro.vn>
2023-05-19 16:09:13 +0700

    router FE

--- app/appv3.1.4/config/routes.php
+++ app/appv3.1.4/config/routes.php
@@ -56,3 +56,15 @@ $route['translate_uri_dashes'] = FALSE;
 $route['san-pham/chi-tiet.html'] = 'sanpham/sanpham/chitiet';
 $route['([a-zA-Z0-9-_]+)/([a-zA-Z0-9-_]+)-p([0-9]+).html'] = 'sanpham/sanpham/chitiet/$1/$2/$3';
 //vi du:  http://dat.dev.vn/tin-tuc/man-city-dai-chien-chelsea-2-dai-gia-lam-tien-so-ke-ai-hon-ai-p48.html
+
+$route['login'] = 'login/login/index';
+$route['login/auth'] = 'login/login/auth';
+
+//tìm kiếm bds
+// $route['dat-nen-du-an/(xa|thi-tran|duong|huong)-([a-zA-Z0-9-_]+)'] = 'bds/search?$=$2&';
+
+
+//tìm kiếm bds
+// datdonganh.vn/{dat-nen-du-an}/xa-{hai-boi}/duong-{van-noi}/gia-{1.5}-ty-{2}-ty/dien-tich-{30}m2-{50}m2/huong-{dong}
+
+// datdonganh.vn/[LOAI_ĐẤT]/[Xã]/[ĐƯỜNG]/gia-[TU_GIA]-[ĐẾN_GIÁ]/[DIEN_TICH]/[HUONG] = 'bds/search/$1/$2/$3/$4/$5/$6'
