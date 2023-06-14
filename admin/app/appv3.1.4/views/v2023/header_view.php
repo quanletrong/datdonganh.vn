6a36aa5984cb4a6ce922c3b32c14622ead36aa53
Le Quan <quanletrong@admicro.vn>
2023-06-14 18:21:55 +0700

    be

--- admin/app/appv3.1.4/views/v2023/header_view.php
+++ admin/app/appv3.1.4/views/v2023/header_view.php
@@ -91,7 +91,7 @@
 		<!-- Main Sidebar Container -->
 		<aside class="main-sidebar sidebar-dark-primary elevation-4">
 			<!-- Brand Logo -->
-			<a href="index3.html" class="brand-link">
+			<a href="#" class="brand-link">
 				<img src="images/logo-vuong.png" alt="Admin Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
 				<span class="brand-text font-weight-light">Admin - Đất đông anh</span>
 			</a>
