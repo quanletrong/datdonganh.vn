0347ba0d02ddfe3f63e6fad158b7a889e903c64c
quanletrong <quanletrong@admicro.vn>
2023-11-21 02:50:11 +0700

    seo

--- app/appv3.1.4/views/v2023/component/footer/footer_view.php
+++ app/appv3.1.4/views/v2023/component/footer/footer_view.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 <div class="container my-5">
-    <img src="https://tpc.googlesyndication.com/simgad/16381994165507055885" class="w-100 img-fluid">
+    <img src="https://tpc.googlesyndication.com/simgad/16381994165507055885" class="w-100 img-fluid" alt="Poster">
 </div>
 <!-- FOOTER -->
 <div class="contaier-fluid" style="background: #f2f2f2;">
